8ea0ab2ed43a227c97d589ca27311a974d52fccd
rupkumar77 <30906897+rupkumar77@users.noreply.github.com>
2017-10-11 23:39:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,24 @@
-console.log('Loaded!');
+//Counter code
+var button=document.getElementById('counter');
+
+button.onclick=function(){
+  
+  //Create a request object
+  var request= new XMLHttpRequest();
+  
+  //Capture the response and store it in a variable
+  request.onreadystatechange= function(){
+    if(request.readyState=== XMLHttpRequest.DONE){
+        //Take Some Action
+        if(request.status === 200){
+            var counter= request.responseText;
+             var span =document.getElementById('count');
+  span.innerHtml=counter.toString();
+        }
+        //Not done yet
+    }  
+  };
+  // Make the request
+  request.open('GET', 'http://rupkumar77.imad.hasura-app.io/counter', true)
+  request.send(null);
+};
